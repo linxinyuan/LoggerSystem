3f6e2d04c93a720c0235813cb94c029f1ad1b460
linxinyuan <linxinyuan@lizhi.fm>
2018-08-08 18:07:37 +0800

    [changed]许慈鸾这只笨猪

--- app/src/main/java/com/lizhi/ls/MainActivity.java
+++ app/src/main/java/com/lizhi/ls/MainActivity.java
@@ -9,28 +9,114 @@ import android.widget.Button;
 
 import com.lizhi.ls.trees.ITree;
 
+import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
-    private Button button;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mPrint_normal_message;
+    private Button mPrint_normal_object;
+    private Button mPrint_bundle_object;
+    private Button mPrint_collection_object;
+    private Button mPrint_intent_object;
+    private Button mPrint_map_object;
+    private Button mPrint_reference_object;
+    private Button mPrint_throwable_object;
+    private Button mPrint_json_message;
+    private Button mPrint_xml_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ITree tag = Logz.tag("Tree");
-                for (int i = 0; i < 2; i++) {
-                    tag.d("good idea");
-                }
-            }
-        });
+        mPrint_normal_message = (Button) findViewById(R.id.print_normal_message);
+        mPrint_normal_object = (Button) findViewById(R.id.print_normal_object);
+        mPrint_bundle_object = (Button) findViewById(R.id.print_bundle_object);
+        mPrint_collection_object = (Button) findViewById(R.id.print_collection_object);
+        mPrint_intent_object = (Button) findViewById(R.id.print_intent_object);
+        mPrint_map_object = (Button) findViewById(R.id.print_map_object);
+        mPrint_reference_object = (Button) findViewById(R.id.print_reference_object);
+        mPrint_throwable_object = (Button) findViewById(R.id.print_throwable_object);
+        mPrint_json_message = (Button) findViewById(R.id.print_json_message);
+        mPrint_xml_message = (Button) findViewById(R.id.print_xml_message);
+
+        mPrint_normal_message.setOnClickListener(this);
+        mPrint_normal_object.setOnClickListener(this);
+        mPrint_bundle_object.setOnClickListener(this);
+        mPrint_collection_object.setOnClickListener(this);
+        mPrint_intent_object.setOnClickListener(this);
+        mPrint_map_object.setOnClickListener(this);
+        mPrint_reference_object.setOnClickListener(this);
+        mPrint_throwable_object.setOnClickListener(this);
+        mPrint_json_message.setOnClickListener(this);
+        mPrint_xml_message.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.print_normal_message:
+                Logz.e("linxiny %s", "test message");
+                break;
+            case R.id.print_normal_object:
+                Logz.e(new Boolean(true));
+                break;
+            case R.id.print_bundle_object:
+                Logz.e(new Bundle());
+                break;
+            case R.id.print_collection_object:
+                printList();
+                break;
+            case R.id.print_intent_object:
+                Logz.e(new Intent());
+                break;
+            case R.id.print_map_object:
+                printMap();
+                break;
+            case R.id.print_reference_object:
+                Logz.e(new SoftReference(0));
+                break;
+            case R.id.print_throwable_object:
+                Logz.e(new NullPointerException("this object is null!"));
+                break;
+            case R.id.print_json_message:
+                printJson();
+                break;
+            case R.id.print_xml_message:
+                printXml();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void printXml() {
+        String xml = "<xyy><test1><test2>key</test2></test1><test3>name</test3><test4>value</test4></xyy>";
+        Logz.xml(xml);
+    }
+
+    private void printJson() {
+        String json = "{'xyy1':[{'test1':'test1'},{'test2':'test2'}],'xyy2':{'test3':'test3'," +
+                "'test4':'test4'}}";
+        Logz.json(json);
+    }
+
+    private void printList() {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add("test" + i);
+        }
+        Logz.e(list);
+    }
+
+    private void printMap() {
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < 5; i++) {
+            map.put("xyy" + i, "test" + i);
+        }
+        Logz.e(map);
     }
 }
